29c6adb24405641db24c467594ac6c93ab63940a
everag <ton.agner@gmail.com>
2018-11-03 23:01:28 -0400

    leetcode - first attempt on https://leetcode.com/problems/counting-bits/description

---
+++ leetcode/338-counting-bits.py
@@ -0,0 +1,23 @@
+class Attempt1:
+    @staticmethod
+    def count_bits(num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(0, num+1):
+            ones = 0
+            power = 0
+            while (2**power) <= i:
+                if i & (2**power) > 0:
+                    ones += 1
+                power += 1
+            res.append(ones)
+        return res
+
+
+assert Attempt1.count_bits(2) == [0, 1, 1]
+assert Attempt1.count_bits(5) == [0, 1, 1, 2, 1, 2]
+
+# 800ms = terrible! need to try again

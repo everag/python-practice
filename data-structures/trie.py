a3b3433d9238b16149a5a394f3a04dac6bbb6c2e
everag <ton.agner@gmail.com>
2018-11-07 21:09:25 -0500

    trie - fixed __str__ bug showing escaped quotes and implemented get_definitions()

--- data-structures/trie.py
+++ data-structures/trie.py
@@ -1,10 +1,19 @@
 class Node:
+    def find_definitions(self, word, defs):
+        if self.meaning is not None:
+            defs[word] = self.meaning
+
+        for letter, child in self.children.items():
+            child.find_definitions(word + letter, defs)
+
+        return defs
+
     def __init__(self):
         self.meaning = None
         self.children = {}
 
     def __str__(self):
-        return '{{meaning:{0}, children:{1}}}'.format(self.meaning, {k: v.__str__() for k, v in self.children.items()})
+        return '{{meaning: {0}, children: {1}}}'.format(self.meaning, {k: v.__str__() for k, v in self.children.items()}.__str__().replace('\'', ''))
 
 
 class Trie:
@@ -21,10 +30,20 @@ class Trie:
             i += 1
         node.meaning = meaning
 
+    def get_definitions(self):
+        return self.root.find_definitions('', {})
+
     def __str__(self):
         return self.root.__str__()
 
 
 trie = Trie()
-trie.add('xunda', 'bunda')
+trie.add('carpa', 'um peixe')
+trie.add('capivara', 'melhor animal')
+trie.add('chão', 'o que te impede de cair infinitamente')
+trie.add('chato', 'minions')
+trie.add('paçoca', 'não começa com c')
+
 print(trie)
+print()
+print({k: v for k, v in trie.get_definitions().items()})
